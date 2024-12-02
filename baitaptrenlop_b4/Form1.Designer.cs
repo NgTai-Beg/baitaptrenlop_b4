a7ccca4d5906463ac3aa1e1ae88348e398f26a2b
Tài Nguyễn Tấn <nguyentantai220804@gmail.com>
2024-12-02 09:39:40 +0700

    Add project files.

---
+++ baitaptrenlop_b4/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace baitaptrenlop_b4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgSv = new System.Windows.Forms.DataGridView();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgSv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgSv
+            // 
+            this.dtgSv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgSv.Location = new System.Drawing.Point(43, 34);
+            this.dtgSv.Name = "dtgSv";
+            this.dtgSv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgSv.Size = new System.Drawing.Size(436, 263);
+            this.dtgSv.TabIndex = 0;
+            this.dtgSv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgSv_CellClick);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(506, 88);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(282, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(506, 143);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(282, 20);
+            this.txtAge.TabIndex = 1;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(506, 34);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(282, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(43, 345);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Them";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(153, 345);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 2;
+            this.btnDel.Text = "Xoa";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(274, 345);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Sua";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(506, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(506, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(506, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.dtgSv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgSv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgSv;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ baitaptrenlop_b4/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace baitaptrenlop_b4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Student> students;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            students = new List<Student>();
+            students.Add(new Student() { Id = 1, Name = "Nguyen Van A", Age = 20 });
+            students.Add(new Student() { Id = 2, Name = "Nguyen Van B", Age = 22 });
+            students.Add(new Student() { Id = 3, Name = "Nguyen Van c", Age = 30 });
+            dtgSv.DataSource = students;
+        }
+
+        private void dtgSv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView dt = (DataGridView)sender;
+            if (e.RowIndex == -1)
+            {
+                return;
+            }
+            DataGridViewRow row = dt.Rows[e.RowIndex];
+            txtId.Text = row.Cells[0].Value.ToString();
+            txtName.Text = row.Cells[1].Value.ToString();
+            txtAge.Text = row.Cells[2].Value.ToString();
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+          students.Add(new Student() {Id = int.Parse(txtId.Text),Name = txtName.Text,Age=int.Parse(txtAge.Text)});
+          dtgSv.DataSource = new BindingList<Student>(students);
+            txtId.Clear();
+            txtName.Clear();
+            txtAge.Clear();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            int currentIndex = dtgSv.CurrentCell.RowIndex;
+            students.RemoveAt(currentIndex);
+            dtgSv.DataSource = new BindingList<Student>(students);
+            txtId.Clear();
+            txtName.Clear();
+            txtAge.Clear();
+
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dtgSv.SelectedRows.Count > 0)
+            {
+                // Lấy chỉ số dòng được chọn
+                int selectedIndex = dtgSv.SelectedRows[0].Index;
+
+                // Cập nhật trực tiếp vào danh sách students
+                students[selectedIndex].Id = int.Parse(txtId.Text); 
+                students[selectedIndex].Name = txtName.Text;
+                students[selectedIndex].Age = int.Parse(txtAge.Text);
+
+                // Làm mới lại DataGridView để hiển thị thay đổi
+                dtgSv.Refresh();
+                MessageBox.Show("Cập nhật thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một dòng để cập nhật!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            txtId.Clear();
+            txtName.Clear();
+            txtAge.Clear();  
+
+
+
+        }
+    }
+}
